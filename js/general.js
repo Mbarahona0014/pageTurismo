18e161651efe8d2d360496fa26a08c3566d94f24
dnisbarahona007 <dnisbarahona007@gmail.com>
2024-09-18 12:43:25 -0600

    FIX GENERAL OTRA VEZ

--- js/general.js
+++ js/general.js
@@ -1,14 +1,14 @@
 const token = "marn_turismo-2024dfsIjf348Jf_-sf39jsH830-3";
 
-/* const url = "https://megabytesv.com/Turismo-MARN";
-const url_landing = "http://localhost/pageTurismo";
-const url_imagenes = "https://megabytesv.com/Turismo-MARN/vista/recursos/turismo"; */
+const url = "https://portaldeb.marn.gob.sv/portal";
+const url_landing = "https://megabytesv.com/pageTurismo";
+const url_imagenes = "https://portaldeb.marn.gob.sv/portal/vista/recursos/turismo";
 
 /* const url = "http://localhost/Turismo-MARN";
 const url_landing = "http://localhost/pageTurismo";
 const url_imagenes = "http://localhost/Turismo-MARN/vista/recursos/turismo";
  */
 
-const url = "https://portaldeb.marn.gob.sv/portal";
+/* const url = "https://portaldeb.marn.gob.sv/portal";
 const url_landing = "https://anp.ambiente.gob.sv";
-const url_imagenes = "https://portaldeb.marn.gob.sv/portal/vista/recursos/turismo";
+const url_imagenes = "https://portaldeb.marn.gob.sv/portal/vista/recursos/turismo"; */
